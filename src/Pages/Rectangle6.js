64ca8413e46f8ec883c68d61729d8e00ebf08b07
Rahul Kumar <rahuliitbhu18@gmail.com>
2022-03-17 18:32:25 +0530

    first commit

--- src/Pages/Rectangle6.js
+++ src/Pages/Rectangle6.js
@@ -5,15 +5,15 @@ const Rectangle6 = () => {
   return (
     <div className='container'>
     <div className='rectangle6 '>
-        <p className='rect4-txt1'>
+        <p className='rect4-txt1' style={{top:"34px"}}>
         Consult with wellness experts
   </p>
-        <p className='rect4-txt2'style={{top:"200px"}}>
+        <p className='rect4-txt2'style={{top:"142px"}}>
         Set up health consultations with experienced doctors and therapists on our app. Simply select a service, answer a few questions, and we’ll connect you to someone who is the right-fit for you. 
 
 
         </p>
-        <a class="rect6-btn waves-effect waves-light btn">Get a consultation</a>
+        <a class="rect6-btn waves-effect waves-light btn" style={{top:"338px"}}>Get a consultation</a>
         
         <>
         <img className='rect6-img1' src={img1}/>
