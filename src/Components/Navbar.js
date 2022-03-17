e3049cc631413e769f0f7b4e2e134f8713608023
Rahul Kumar <rahuliitbhu18@gmail.com>
2022-03-17 19:12:46 +0530

    first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 <nav className='rectangle'>
     <div className="nav-wrapper">
       <a href="#" className="brand-logo left black-text" style={{left:"24px"}} ><i className="material-icons small" >dehaze</i> </a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <ul id="nav-mobile" class="right ">
         <li><a className='black-text' href="sass.html"><i className="material-icons small" >shopping_cart</i></a></li>
         <li><a className='black-text' href="sass.html"><i className="material-icons small" >notifications_none</i></a></li>
         
